f516904fe6544aa18b6f0eb4cc5f43232e3943a2
YChaeeun <se_same5315@naver.com>
2020-03-20 02:12:25 +0900

    feat-#3 [crawling] crawling comments

--- work.py
+++ work.py
@@ -2,13 +2,12 @@ import userInfo as u
 import siteInfo as s
 
 from selenium import webdriver as wd
-import urllib
 
 from datetime import datetime
+import time
 
 user = u.User()  # 유저의 아이디와 비밀번호 정보
 site = s.Site()  # 접속하려는 사이트와 관련된 정보 (ex. url, css, class, id)
-
 
 # 제어할 웹 브라우저 열기
 driver = wd.Chrome("./tools/chromedriver.exe")
@@ -49,4 +48,38 @@ driver.find_element_by_css_selector("#tinymce").send_keys(site.content)
 ### 작성완료!
 #driver.find_element_by_css_selector(".site_button_reverse").click()
 
+# -------------------------------------------------- #
+
+# -------------------------------------------------- #
+# 3. 댓글 크롤링
+driver.execute_script("window.history.go(-1)")
+
+### 가장 최근에 작성한 <출결체크> 게시글 접근
+title = driver.find_elements_by_css_selector(".bbslist tbody .left a")
+length = len(driver.find_elements_by_css_selector(".bbslist tbody"))
+
+for i in range(length):
+    if ("<" in title[i].text) :
+        title[i].click()
+        break
+
+### 16시 31분이 되면 전체 댓글 수집 (출석)
+while True :
+    now = datetime.now()
+    if (now.hour == 16) and (now.minute >= 30) :
+        comment_list = driver.find_elements_by_css_selector(".commentbox .comment-list .comment-name")
+        break
+    else :
+        driver.refresh()
+        time.sleep(60)
+
+### 지각
+for i in range(20) :
+    now = datetime.now()
+    if (now.hour == 16) and (now.minute >= 45) :
+        comment_list_late = driver.find_elements_by_css_selector(".commentbox .comment-list .comment-name")
+        break
+    else :
+        driver.refresh()
+        time.sleep(60)
 # -------------------------------------------------- #
