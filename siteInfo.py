acd8684f448402677942a36fa69b5e0d90cef21d
YChaeeun <se_same5315@naver.com>
2020-03-19 23:14:42 +0900

    feat-#2 [create post] example of Site class

--- siteInfo.py
+++ siteInfo.py
@@ -5,6 +5,9 @@ class Site :
         self.__id_password = "usr_pwd"
         self.__xpath_login = '//*[@id="login_btn"]'
 
+        self.__title = ""
+        self.__content = ""
+
     @property
     def url(self) :
         return self.__url
@@ -20,3 +23,15 @@ class Site :
     @property
     def btnLogin(self) :
         return self.__xpath_login
+
+    @property
+    def title(self) :
+        return self.__title
+
+    @property
+    def content(self) :
+        return self.__content
+
+    @title.setter
+    def title(self, date) :
+        self.__title = f"<출석 체크> {date}"
