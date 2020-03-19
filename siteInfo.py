3f4c6571a497ef244c6cbb595856671a5d5e988c
YChaeeun <se_same5315@naver.com>
2020-03-20 02:26:40 +0900

    feat-#3 [crawling] example of Site class

--- siteInfo.py
+++ siteInfo.py
@@ -5,8 +5,11 @@ class Site :
         self.__id_password = "usr_pwd"
         self.__xpath_login = '//*[@id="login_btn"]'
 
-        self.__title = ""
-        self.__content = ""
+        self.__title = "<출석 체크>"
+        self.__content = "댓글로 출석체크"
+
+        self.__title_complete = "출결 결과"
+        self.__content_complete = "지각 - \n결석 - "
 
     @property
     def url(self) :
@@ -32,6 +35,23 @@ class Site :
     def content(self) :
         return self.__content
 
+    @property
+    def title_complete(self) :
+        return self.__title_complete
+
+    @property
+    def content_complete(self) :
+        return self.__content_complete
+
     @title.setter
     def title(self, date) :
         self.__title = f"<출석 체크> {date}"
+
+    @title_complete.setter
+    def title_complete(self, date) :
+        self.__title_complete = f"{date} 출결 결과"
+
+    @content_complete.setter
+    def content_complete(self, vals) :
+        late, absent = vals
+        self.__content_complete = f"지각 - {late}\n결석 - {absent}"
